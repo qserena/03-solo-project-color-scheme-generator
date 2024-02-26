d57df20f64b30bd729659893acc09db01091d12c
Tom Serenander <tom@serenander.se>
2024-02-26 10:18:26 +0100

    Added initial colors at the loading of the site.

--- index.js
+++ index.js
@@ -16,6 +16,32 @@ getBtn.addEventListener('click', function () {
 		})
 })
 
+function initScreen() {
+	let html = `
+            <div class="color">
+                <div class="color-part" style="background-color: #B4DC23"></div>
+                <div class="hex-part">#B4DC23</div>
+            </div>
+			<div class="color">
+                <div class="color-part" style="background-color: #C4E44E"></div>
+                <div class="hex-part">#C4E44E</div>
+            </div>
+			<div class="color">
+                <div class="color-part" style="background-color: #D3EC79"></div>
+                <div class="hex-part">#D3EC79</div>
+            </div>
+			<div class="color">
+                <div class="color-part" style="background-color: #CCCCCC"></div>
+                <div class="hex-part">#CCCCCC</div>
+            </div>
+			<div class="color">
+                <div class="color-part" style="background-color: #E6E6E6"></div>
+                <div class="hex-part">#E6E6E6</div>
+            </div>
+        `
+	colorsEl.innerHTML = html
+}
+
 function renderColors() {
 	let html = ''
 	for (let color of colors) {
@@ -28,3 +54,5 @@ function renderColors() {
 	}
 	colorsEl.innerHTML = html
 }
+
+initScreen()
